250a51d0176c3d12ebd739c2c4690fa24b675113
JC Ivancevich <jc.ivancevich@gmail.com>
2017-11-29 17:01:23 -0300

    usar configuracion de Hibernate en XML

--- RestaurantServer/src/server/Server.java
+++ RestaurantServer/src/server/Server.java
@@ -1,86 +1,14 @@
 package server;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
-import entities.AreaEntity;
-import entities.BarraEntity;
-import entities.CafeteriaEntity;
-import entities.CajaEntity;
-import entities.CartaEntity;
-import entities.CocinaEntity;
-import entities.ComandaEntity;
-import entities.ComisionEntity;
-import entities.DepositoAreaEntity;
-import entities.DepositoEntity;
-import entities.DepositoGeneralEntity;
-import entities.DepositoRestaurantEntity;
-import entities.DirectoEntity;
-import entities.EmpleadoEntity;
-import entities.FacturaEntity;
-import entities.FormaPagoEntity;
-import entities.InsumoEntity;
-import entities.InsumoProductoEntity;
-import entities.ItemCartaEntity;
-import entities.ItemFacturaEntity;
-import entities.ItemPedidoEntity;
-import entities.MesaCompuestaEntity;
-import entities.MesaEntity;
-import entities.MesaSimpleEntity;
-import entities.PedidoEntity;
-import entities.PedidoReposicionEntity;
-import entities.ProductoEntity;
-import entities.ProveedorEntity;
-import entities.ReservaEntity;
-import entities.RolEntity;
-import entities.SectorSalonEntity;
-import entities.SemiElaboradoEntity;
-import entities.SucursalEntity;
-import entities.TareaEntity;
+import interfaces.MesasTDA;
+import rmi.MesasManager;
 
 public class Server {
-	
-	public static void main(String[] args) {
-		AnnotationConfiguration config = new AnnotationConfiguration();
-		config.addAnnotatedClass(AreaEntity.class);
-		config.addAnnotatedClass(BarraEntity.class);
-		config.addAnnotatedClass(CafeteriaEntity.class);
-		config.addAnnotatedClass(CajaEntity.class);
-		config.addAnnotatedClass(CartaEntity.class);
-		config.addAnnotatedClass(ItemCartaEntity.class);
-		config.addAnnotatedClass(ProductoEntity.class);
-		config.addAnnotatedClass(InsumoProductoEntity.class);
-		config.addAnnotatedClass(InsumoEntity.class);
-		config.addAnnotatedClass(ProveedorEntity.class);
-		config.addAnnotatedClass(CocinaEntity.class);
-		config.addAnnotatedClass(ComandaEntity.class);
-		config.addAnnotatedClass(ItemPedidoEntity.class);
-		config.addAnnotatedClass(ComisionEntity.class);
-		config.addAnnotatedClass(DepositoAreaEntity.class);
-		config.addAnnotatedClass(DepositoEntity.class);
-		config.addAnnotatedClass(DepositoGeneralEntity.class);
-		config.addAnnotatedClass(EmpleadoEntity.class);
-		config.addAnnotatedClass(PedidoReposicionEntity.class);
-		config.addAnnotatedClass(RolEntity.class);
-		config.addAnnotatedClass(DepositoRestaurantEntity.class);
-		config.addAnnotatedClass(DirectoEntity.class);
-		config.addAnnotatedClass(FacturaEntity.class);
-		config.addAnnotatedClass(FormaPagoEntity.class);
-		config.addAnnotatedClass(ItemFacturaEntity.class);
-		config.addAnnotatedClass(MesaCompuestaEntity.class);
-		config.addAnnotatedClass(SectorSalonEntity.class);
-		config.addAnnotatedClass(MesaEntity.class);
-		config.addAnnotatedClass(MesaSimpleEntity.class);
-		config.addAnnotatedClass(PedidoEntity.class);
-		config.addAnnotatedClass(ReservaEntity.class);
-		config.addAnnotatedClass(SemiElaboradoEntity.class);
-		config.addAnnotatedClass(SucursalEntity.class);
-		config.addAnnotatedClass(TareaEntity.class);
-		SessionFactory session = config.buildSessionFactory();
-	}
-	
-	
-/*	MesasTDA mesasRemoteObject;
+
+	MesasTDA mesasRemoteObject;
 
 	public Server() {
 		start();
@@ -88,14 +16,13 @@ public class Server {
 
 	private void start() {
 		try {
-			//LocateRegistry.createRegistry(1099);
-			//mesasRemoteObject = new MesasManager();
-			//Naming.rebind("//localhost/mesas", mesasRemoteObject);
-			//System.out.println("Binded to //localhost/mesas");
-			
-			
+			LocateRegistry.createRegistry(1099);
+			mesasRemoteObject = new MesasManager();
+			Naming.rebind("//localhost/mesas", mesasRemoteObject);
+			System.out.println("Binded to //localhost/mesas");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}*/
+	}
+
 }
