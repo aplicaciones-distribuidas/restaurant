05415c4926f766bd6cd7e6478288d893ed4941ff
JC Ivancevich <jc.ivancevich@gmail.com>
2017-12-09 13:45:56 -0300

    botones para acciones en mesas ocupadas

--- RestaurantClient/src/gui/MesasOcupadasLista.java
+++ RestaurantClient/src/gui/MesasOcupadasLista.java
@@ -3,32 +3,29 @@ package gui;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyVetoException;
 import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import dto.MesaOcupacionView;
 import dto.MesaView;
 
+import javax.swing.*;
+
 public class MesasOcupadasLista extends JInternalFrame {
 	private static final long serialVersionUID = -644489175035527974L;
-	// private JInternalFrame aux;
+	private MesasOcupadasLista aux;
 
 	public MesasOcupadasLista(List<MesaOcupacionView> mesas) {
 		super("Mesas Ocupadas", true, true, false, true);
 		configurar(mesas);
 		this.setVisible(true);
 		this.pack();
-		// aux = this;
+		aux = this;
 	}
 
 	private void configurar(List<MesaOcupacionView> mesasOcupacion) {
 		JPanel p = new JPanel();
-		p.setLayout(new GridLayout(mesasOcupacion.size() + 1, 4));
+		p.setLayout(new GridLayout(mesasOcupacion.size() + 1, 6));
 
 		JLabel lblTituloNumero = new JLabel("Mesas");
 		p.add(lblTituloNumero);
@@ -36,8 +33,14 @@ public class MesasOcupadasLista extends JInternalFrame {
 		JLabel lblTituloSectorSalon = new JLabel("Sector Salón");
 		p.add(lblTituloSectorSalon);
 
+		JLabel lblTituloProductos = new JLabel("Productos");
+		p.add(lblTituloProductos);
+
 		JLabel lblTituloCerrar = new JLabel("Cerrar");
 		p.add(lblTituloCerrar);
+
+		JLabel lblTituloCobrar = new JLabel("Cobrar");
+		p.add(lblTituloCobrar);
 
 		JLabel lblTituloLiberar = new JLabel("Liberar");
 		p.add(lblTituloLiberar);
@@ -54,23 +57,43 @@ public class MesasOcupadasLista extends JInternalFrame {
 			p.add(new JLabel(mesasAsignadas));
 			p.add(new JLabel(sectorSalon));
 
+			JButton btnProductos = new JButton("Agregar");
+			p.add(btnProductos);
+
 			JButton btnCerrar = new JButton("Cerrar");
 			p.add(btnCerrar);
+
+			JButton btnCobrar = new JButton("Cobrar");
+			p.add(btnCobrar);
 
 			JButton btnLiberar = new JButton("Liberar");
 			p.add(btnLiberar);
 
+			btnProductos.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Agregar productos a mesa " + mesaOcupacion.toString());
+				}
+			});
+
 			btnCerrar.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					System.out.println(mesaOcupacion.toString());
+					System.out.println("Cerrar mesa " + mesaOcupacion.toString());
+				}
+			});
+
+			btnCobrar.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Cobrar mesa " + mesaOcupacion.toString());
 				}
 			});
 
 			btnLiberar.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					System.out.println(mesaOcupacion.toString());
+					System.out.println("Liberar mesa " + mesaOcupacion.toString());
 				}
 			});
 		}
@@ -79,5 +102,14 @@ public class MesasOcupadasLista extends JInternalFrame {
 
 		this.setContentPane(p);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void cerrar() {
+		try {
+			this.setClosed(true);
+		} catch (PropertyVetoException ex) {
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Error al cerrar ventana");
+		}
 	}
 }
