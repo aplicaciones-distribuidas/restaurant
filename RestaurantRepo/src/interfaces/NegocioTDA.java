ed3bff6e3f1b7c53ebcbf5a52dbe161ea31aa3f7
JC Ivancevich <jc.ivancevich@gmail.com>
2017-12-08 21:33:39 -0300

    format tda

--- RestaurantRepo/src/interfaces/NegocioTDA.java
+++ RestaurantRepo/src/interfaces/NegocioTDA.java
@@ -23,65 +23,32 @@ public interface NegocioTDA extends Remote {
 
 	// NO SE QUE SE SUPONEN QUE TIENEN QUE HACER ESTOS 3 METODOS
 	public int stockPlatoBebida() throws RemoteException;
-
 	public void centralizacionCompras() throws RemoteException;
-
 	public void centralizacionDistribucion() throws RemoteException;
 
 	public void asignarHorasTrabajo(int horas, Long trabajoId) throws RemoteException, TareaNoExisteException;
-
 	public void reservar(String sucursal, int cantPersonas, Date fecha) throws RemoteException;
-
-	public List<MesaView> mesasDisponibles(String sucursal, int cantPersonas) throws RemoteException,
-			SucursalNoExisteException, BaseDeDatosException;
-
-	public List<MesaOcupacionView> mesasOcupadas(String sucursal) throws RemoteException, SucursalNoExisteException,
-			BaseDeDatosException;
-
+	public List<MesaView> mesasDisponibles(String sucursal, int cantPersonas) throws RemoteException, SucursalNoExisteException, BaseDeDatosException;
+	public List<MesaOcupacionView> mesasOcupadas(String sucursal) throws RemoteException, SucursalNoExisteException, BaseDeDatosException;
 	public void asignarEmpleadoSectorSucursal(Long idEmpleado, String sucursal, int numero) throws RemoteException;
-
 	public EmpleadoView buscarEmpleado(String nombre, String apellido, String rol) throws RemoteException;
-
-	public MesaOcupacionView abrirMesa(String sucursal, int cantPersonas, int idEmpleado) throws RemoteException,
-			NoHayMesasDisponiblesException;
-
+	public MesaOcupacionView abrirMesa(String sucursal, int cantPersonas, int idEmpleado) throws RemoteException, NoHayMesasDisponiblesException;
 	public void lanzarPedido(String sucursal, int idMesaOcupacion) throws RemoteException;
-
 	public void realizarReclamo(String sucursal, int idMesaOcupacion, String reclamo) throws RemoteException;
-
 	public void marcarComandaRealizada(int nroPedido) throws RemoteException;
-
 	public void facturarMesa(String sucursal, int idMesaOcupacion) throws RemoteException;
-
 	public PedidoReposicionView buscarPedidoReposicion() throws RemoteException;
-
 	public PedidoView buscarPedido() throws RemoteException;
-
 	public ReporteView generarReporteReposicionInsumos() throws RemoteException;
-
 	public void verificarStock() throws RemoteException;
-
 	public void generarOrdenCompra() throws RemoteException;
-
 	public void actualizarStock(Long idProducto, int cantidad) throws RemoteException;
-
 	public void cerrarMesa(String sucursal, int idMesaOcupacion) throws RemoteException;
-
 	public void liberarMesa(String sucursal, int idMesaOcupacion) throws RemoteException;
-
 	public void registrarCobro(String sucursal, int idMesaOcupacion) throws RemoteException;
 
 	// PARA PRESENTACION TPO SEGUN DOC DE GODIO
-
-	// TODO: el proveedor hay que meterlo en el producto (abstracto) y modificar la
-	// entity tambien!, el crear plato (no semielaborado) tiene que recibir el
-	// proveedor tambien, solo el nombre y validar que exista
-	// sino tirar ProveedorNoExisteException
-	public void crearPlato(String rubro, int caducidad, float comisionMozo, Date fecha, float precio, String nombreArea)
-			throws RemoteException, AreaNoExisteException;
-
-	public void crearPlatoSemielaborado(String rubro, int caducidad, float comisionMozo, Date fecha, float precio,
-			String nombreArea, List<InsumoProductoView> insumos) throws RemoteException, AreaNoExisteException,
-			ProveedorNoExisteException;
+	public void crearPlato(String rubro, int caducidad, float comisionMozo, Date fecha, float precio, String nombreArea) throws RemoteException, AreaNoExisteException;
+	public void crearPlatoSemielaborado(String rubro, int caducidad, float comisionMozo, Date fecha, float precio, String nombreArea, List<InsumoProductoView> insumos) throws RemoteException, AreaNoExisteException, ProveedorNoExisteException;
 
 }
